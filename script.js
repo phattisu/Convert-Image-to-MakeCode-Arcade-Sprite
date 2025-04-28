e66957d583369c6dc2ce92ba4789006aee315180
Phattisu <naphatism@hotmail.com>
2025-04-28 16:36:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -474,11 +474,13 @@ async function convert(imgElement, frameImageData = null, frameIndex = 0) {
                     } else if (distBg === 0) {
                         percentage = 0; // Exactly matches Background
                     } else {
+			if (distFg > distBg) {
 			percentage = distFg / (distFg + distBg); // Closer to BG means smaller distBg, smaller percentage (closer to BG color)
-			                             // Closer to FG means smaller distFg, larger percentage (closer to FG color)
-                                                              // Let's flip this: distFg / (distFg + distBg) -> Higher % means closer to FG
+			}                                        // Closer to FG means smaller distFg, larger percentage (closer to FG color)
+                        else {                                   // Let's flip this: distFg / (distFg + distBg) -> Higher % means closer to FG
                         percentage = distBg / (distFg + distBg); // Higher % means closer to FG color
-		    }
+			}
+		}
 
                     // Clamp percentage between 0 and 1
                     percentage = Math.max(0, Math.min(1,percentage));
